d6b875858ebf576d13fce52ceb9e48e0d40b9a2a
I-am-Harshdeep <beyourselfharsh@gmail.com>
2019-09-15 22:11:59 +0530

    adding project

---
+++ smart-brain-apit/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image= require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'Harshking',
+      database : 'smartbrain'
+    }
+  });
+
+//db.select('*').from('users').then(data => {
+//    console.log(data);
+//});
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+/*const database = {
+    users:[
+        {
+            id: '123',
+            name: 'Jassi',
+            email: 'Jassi@gmail.com',
+            entries: 0,
+            password: 'cookies',
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'Sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
+        }
+    ]
+}*/
+
+app.get('/', (req,res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })      
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port 3000`);
+});
